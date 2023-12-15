231e440e4a0f1151cab231caabf053a4b92956f0
greedo7 <nathaniel.decarie@stu.ocsb.ca>
2023-12-15 11:14:23 -0500

    i tried

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -1,9 +1,66 @@
+import java.util.Scanner;
 
 public class Conditionals {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		// start reading in questions
+		System.out.println("respond 2 every question with Y or N please\n");
+		
+		System.out.println("i like to write my own stories");
+		String q1 = in.nextLine().toUpperCase();
+		if (q1.startsWith("Y")) {
+			
+		}
+		System.out.println("i like to take pictures");
+		String q2 = in.nextLine().toUpperCase();
+		System.out.println("i like to plant seeds and grow plants");
+		String q3 = in.nextLine().toUpperCase();
+		System.out.println("i like to count");
+		String q4 = in.nextLine().toUpperCase();
+		System.out.println("i like to check the weather");
+		String q5 = in.nextLine().toUpperCase();
+		System.out.println("i like to talk to others about ideas");
+		String q6 = in.nextLine().toUpperCase();
+		System.out.println("i like to read in my free time");
+		String q7 = in.nextLine().toUpperCase();
+		System.out.println("i like to dance");
+		String q8 = in.nextLine().toUpperCase();
+		System.out.println("i like to read maps");
+		String q9 = in.nextLine().toUpperCase();
+		System.out.println("i like to figure out patterns");
+		String q10 = in.nextLine().toUpperCase();
+		System.out.println("i like to take turns");
+		String q11 = in.nextLine().toUpperCase();
+		System.out.println("i like to solve puzzles");
+		String q12 = in.nextLine().toUpperCase();
+		System.out.println("i like to sing");
+		String q13 = in.nextLine().toUpperCase();
+		System.out.println("i like to make things with my hands");
+		String q14 = in.nextLine().toUpperCase();
+		System.out.println("i like to talk to others");
+		String q15 = in.nextLine().toUpperCase();
+		System.out.println("i like poetry and rhyming");
+		String q16 = in.nextLine().toUpperCase();
+		System.out.println("i like to be by myself sometimes");
+		String q17 = in.nextLine().toUpperCase();
+		System.out.println("i like to touch different materials");
+		String q18 = in.nextLine().toUpperCase();
+		System.out.println("i like to move around a lot");
+		String q19 = in.nextLine().toUpperCase();
+		System.out.println("i like to play alone");
+		String q20 = in.nextLine().toUpperCase();
+		System.out.println("i like to play word games");
+		String q21 = in.nextLine().toUpperCase();
+		System.out.println("i like taking care of animals");
+		String q22 = in.nextLine().toUpperCase();
+		System.out.println("i like to draw");
+		String q23 = in.nextLine().toUpperCase();
+		System.out.println("i am wise");
+		String q24 = in.nextLine().toUpperCase();
 
+		
+		
 	}
 
 }
---
+++ src/ForLoops.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ForLoops {
+
+	public static void main(String[] args) {
+			Scanner in = new Scanner(System.in);
+			System.out.println("enter a number, see if it's prime or not!");
+			int userNum = in.nextInt();
+			
+			for (int x = 1; userNum % x == 2; x++) {
+				
+			}
+	}
+
+}
